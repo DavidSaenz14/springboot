739b23a42b1734ad510ba6e25d1201dcff43a1c7
DavidSaenz14 <122973332+DavidSaenz14@users.noreply.github.com>
2024-11-25 16:38:11 +0100

    Update EquipoController.java

--- src/main/java/com/example/springboot/controllers/EquipoController.java
+++ src/main/java/com/example/springboot/controllers/EquipoController.java
@@ -1,10 +1,9 @@
 package com.example.springboot.controllers;
 
+
 import com.example.springboot.models.Equipo;
 import com.example.springboot.services.EquipoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,44 +18,28 @@ public class EquipoController {
     private EquipoService equipoService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<Equipo> getEquipo(@PathVariable Long id) {
-        Optional<Equipo> equipo = equipoService.findEquipoById(id);
-        if (equipo.isPresent()) {
-            return ResponseEntity.ok(equipo.get());
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
+    public Optional<Equipo> getEquipo(@PathVariable Long id) {
+        return equipoService.findEquipoById(id);
     }
 
     @GetMapping
-    public ResponseEntity<List<Equipo>> getAllEquipos() {
-        List<Equipo> equipos = equipoService.findAllEquipos();
-        if (!equipos.isEmpty()) {
-            return ResponseEntity.ok(equipos);
-        } else {
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
-        }
+    public List<Equipo> getAllEquipos() {
+        return equipoService.findAllEquipos();
     }
 
     @PostMapping
-    public ResponseEntity<Equipo> createEquipo(@RequestBody Equipo equipo) {
-        Equipo savedEquipo = equipoService.saveEquipo(equipo);
-        return ResponseEntity.status(HttpStatus.CREATED).body(savedEquipo);
+    public Equipo createEquipo(@RequestBody Equipo equipo) {
+        return equipoService.saveEquipo(equipo);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Equipo> updateEquipo(@PathVariable Long id, @RequestBody Equipo equipo) {
+    public Equipo updateEquipo(@PathVariable Long id, @RequestBody Equipo equipo) {
         equipo.setId(id);
-        Equipo updatedEquipo = equipoService.saveEquipo(equipo);
-        return ResponseEntity.ok(updatedEquipo);
+        return equipoService.saveEquipo(equipo);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteEquipo(@PathVariable Long id) {
-        if (!equipoService.existsById(id)) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
+    public void deleteEquipo(@PathVariable Long id) {
         equipoService.deleteEquipoById(id);
-        return ResponseEntity.ok().build();
     }
 }
