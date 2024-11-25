c5a789f74b46556727e1caa022978effe24c710f
DavidSaenz14 <122973332+DavidSaenz14@users.noreply.github.com>
2024-11-25 16:07:15 +0100

    Update EquipoController.java

--- src/main/java/com/example/springboot/controllers/EquipoController.java
+++ src/main/java/com/example/springboot/controllers/EquipoController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/equipos")
-@CrossOrigin("*")
+@CrossOrigin("origins = "https://tfg-futbol.vercel.app"")
 public class EquipoController {
 
     @Autowired
