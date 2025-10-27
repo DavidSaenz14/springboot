ee9dafcd155d3e2d24ec65d5eb03477d7afdf926
DavidSaenz14 <dam115.2023@gmail.com>
2025-10-27 09:17:50 +0100

    Dar permisos de ejecución al wrapper de Maven

--- src/main/java/com/example/springboot/controllers/EquipoController.java
+++ src/main/java/com/example/springboot/controllers/EquipoController.java
@@ -1,45 +1,4 @@
 package com.example.springboot.controllers;
 
-
-import com.example.springboot.models.Equipo;
-import com.example.springboot.services.EquipoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
-
-@RestController
-@RequestMapping("/equipos")
-@CrossOrigin("*")
 public class EquipoController {
-
-    @Autowired
-    private EquipoService equipoService;
-
-    @GetMapping("/{id}")
-    public Optional<Equipo> getEquipo(@PathVariable Long id) {
-        return equipoService.findEquipoById(id);
-    }
-
-    @GetMapping
-    public List<Equipo> getAllEquipos() {
-        return equipoService.findAllEquipos();
-    }
-
-    @PostMapping
-    public Equipo createEquipo(@RequestBody Equipo equipo) {
-        return equipoService.saveEquipo(equipo);
-    }
-
-    @PutMapping("/{id}")
-    public Equipo updateEquipo(@PathVariable Long id, @RequestBody Equipo equipo) {
-        equipo.setId(id);
-        return equipoService.saveEquipo(equipo);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteEquipo(@PathVariable Long id) {
-        equipoService.deleteEquipoById(id);
-    }
 }
